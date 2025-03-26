c92a5ea9ee2fdf1baff96d2322ce8349f6e548b8
shitalch17 <135641001+shitalch17@users.noreply.github.com>
2025-03-26 16:28:27 +0530

    Update headerComponent.js

--- src/components/headerComponent.js
+++ src/components/headerComponent.js
@@ -3,20 +3,30 @@ import { Link } from 'react-router-dom'
 const HeaderComponent = () => {
   return (
     <div>
-      <header >
-      
-        <nav className='navbar navbar-expand-md navbar-dark bg-dark' >
-        <h1 style={{color:"white"}}>Employee Management System<br/><br/></h1><hr/>
-        <nav style={{marginLeft:-550}}>
-            <br/>
-         <Link to='/' style={{marginRight:-750,fontSize:20}}>EmployeeList</Link>&nbps;&nbps;
-         <Link to='/employees/addemp' style={{marginLeft:700,fontSize:20}}>Add Employee</Link>
-         {/* <Link to='/addproduct' style={{marginLeft:50,fontSize:20}}>Add Product</Link> */}
-         {/* <Link to='/listproduct' style={{marginLeft:50,fontSize:20}}>List Product</Link> */}
-         </nav>
-        </nav>
-
-      </header>
+      <header>
+        {/* Employee Management System Title - Outside Navbar */}
+      <div className="text-center bg-dark text-white py-3">
+        <h1 className="fs-4 m-0">Employee Management System</h1>
+      </div>
+      <nav className='navbar navbar-expand-lg navbar-dark bg-dark px-3'>
+        <div className='container-fluid'>
+          {/* <h1 className='navbar-brand mb-0 text-white fs-4'>Employee Management System</h1> */}
+          <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav'>
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div className='collapse navbar-collapse justify-content-end' id='navbarNav'>
+            <ul className='navbar-nav me-auto'>
+              <li className='nav-item'>
+                <Link className='nav-link text-white fs-5' to='/'>Employee List</Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link text-white fs-5' to='/employees/addemp'>Add Employee</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
     </div>
   )
 } 
